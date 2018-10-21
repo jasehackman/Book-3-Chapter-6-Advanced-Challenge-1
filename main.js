59d31d25a168def9f2aea00787991048d0c1db2f
jasehackman <jasehackman@gmail.com>
2018-10-20 22:33:39 -0500

    completed advanced challenge2

--- main.js
+++ main.js
@@ -12,10 +12,40 @@
 // console.log(list)
 
 const theSpot = document.querySelector(".message");
+const frag = document.createDocumentFragment();
 for (let i = 0; i < 5; i++) {
-
 let item = document.createElement("section");
 item.className = "message";
 item.textContent = "stuff";
-theSpot.appendChild(item);
+frag.appendChild(item);
 }
+
+theSpot.appendChild(frag);
+
+
+
+// // A new "virtual" document fragment to contain components
+// const fragment = document.createDocumentFragment()
+
+// // Create an emperor component element
+// const julius = document.createElement('div')
+// julius.textContent = "Julius Caesar"
+// fragment.appendChild(julius)
+
+// // Create an emperor component element
+// const augustus = document.createElement('div')
+// augustus.textContent = "Augustus Caesar"
+// fragment.appendChild(augustus)
+
+// // Create an emperor component element
+// const aurelius = document.createElement('div')
+// aurelius.textContent = "Marcus Aurelius"
+// fragment.appendChild(aurelius)
+
+// /*
+//     Now I can update my HTML document all at once, with all
+//     three emperor components rendered. Otherwise, I would
+//     have needed to add each one, individually - a much more
+//     "expensive" operation.
+// */
+// document.querySelector("#emperorList").appendChild(fragment)
